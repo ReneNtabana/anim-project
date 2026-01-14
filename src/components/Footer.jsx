a60477737a291a6695a15664487a87f0a5db1735
ReneNtabana <ntabanarene205@gmail.com>
2026-01-14 18:25:20 +0200

    feat: applying sections animations and final touches

---
+++ src/components/Footer.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import { FaGithub, FaTwitter, FaDiscord, FaLinkedin } from 'react-icons/fa'
+
+const links = [
+    {
+        icon: <FaDiscord />,
+        href: "https://Discord.com"
+    },
+    {
+        icon: <FaTwitter />,
+        href: "https://twitter.com"
+    },
+    {
+        icon: <FaGithub />,
+        href: "https://github.com"
+    },
+    {
+        icon: <FaLinkedin />,
+        href: "https://linkedin.com"
+    }
+]
+
+const Footer = () => {
+  return (
+    <footer className='w-screen bg-violet-300 py-4 text-black'>
+        <div className=' container mx-auto flex flex-col items-center justify-between gap-4 px-4 md:flex-row'>
+            <p className='text-center text-sm md:text-left'>©NOVA 2024. All rights reserved.</p>
+            <div className='flex justify-center gap-4 md:justify-start'>
+                {links.map((link) => (
+                    <a key={link} href={link.href} target='_blank' className='text-black hover:text-white transition-colors duration-500 ease-in-out'>
+                        {link.icon}
+                    </a>
+                ))}
+            </div>
+            <a href='privacy-policy' className='text-center text-sm hover:underline md:text-right'>Privacy Policy</a>
+        </div>
+    </footer>
+  )
+}
+
+export default Footer
